8547310c3a4129b6d2f09054bfe2012cb3f2cdbd
evlazarev007 <115842128+evlazarev007@users.noreply.github.com>
2022-12-13 17:31:59 +0600

    1

--- functions.sh
+++ functions.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-# Put your code here
+pow () {
+	echo "$(($1**$2))"
+}
+
+shortest () {
+read -r -a array <<< "$@"
+local min=${#array[i]}
+for ((i=0;i<${#array[@]};i++)); do
+	if [[ ${#array[i]} -lt $min  ]]; then
+		min=${#array[i]}
+	fi
+done
+
+for ((i=0;i<${#array[@]};i++)); do
+	if [[ ${#array[i]} -eq $min  ]]; then
+		echo "${array[i]}"
+	fi
+done
+}
+
+print_log () {
+	echo "[$(date +%Y-%m-%d) $(date +%H:%M)] $@"
+}
